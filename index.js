fec95afe6dcc480b37640c466aa47b2e8f240e67
Sadie <an5dy90@gmail.com>
2024-07-24 01:30:17 -0400

    changed the session({x},x, x) to session({x: ccc, x:true, x:true})

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const authenticatedUser = (username, password) => {
 
 const app = express();
 
-app.use(session({secret:"fingerpint"},resave=true,saveUninitialized=true));
+app.use(session({secret:"fingerpint",resave:true, saveUninitialized:true}));
 
 app.use(express.json());
 
