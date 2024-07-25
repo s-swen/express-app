c01604256764df50f4c5403dabd416a58f3c92de
Sadie <an5dy90@gmail.com>
2024-07-25 00:57:29 -0400

    added get-email endpoint"

--- router/friends.js
+++ router/friends.js
@@ -11,13 +11,14 @@ let friends = {
 
 // GET request: Retrieve all friends
 router.get("/",(req,res)=>{
-  res.send(JSON.stringify(friends));//This line is to be replaced with actual return value
+  res.send(JSON.stringify(friends, null, 4));//This line is to be replaced with actual return value
 });
 
 // GET by specific ID request: Retrieve a single friend with email ID
 router.get("/:email",(req,res)=>{
   // Update the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  res.send(friends[email]);
 });
 
 
